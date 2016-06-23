34764e0622b8d09aba897909556f978cf7afab65
martinbee <martin@poeticsystems.com>
2016-06-23 16:41:54 -0500

    set up todos schema; add custom resolver with examples;

--- imports/data/resolvers.js
+++ imports/data/resolvers.js
@@ -1,11 +1,52 @@
-import Instagram from '/imports/data/instagram-connector';
+// stub data
+const TODOs = [
+  {
+    "id": 1,
+    "title": "Read emails",
+    "completed": false
+  },
+  {
+    "id": 2,
+    "title": "Buy orange",
+    "completed": false
+  },
+  {
+    "id": 3,
+    "title": "Buy Apple Products",
+    "completed": false
+  },
+  {
+    "id": 4,
+    "title": "Walk cat",
+    "completed": true
+  }
+];
 
+// example resolvers, depend upon connector logic
 const resolvers = {
-  Query: {
-    async data(root, { keywords }) {
-      return Instagram.getRecent(keywords);
+  TodosSchema: {
+    todosExampleResolveFunction() {
+      return TODOs;
+    },
+    firebaseResolveFunction() {
+      return firebase.getTodos();
+    },
+    googleSheetsResolveFunction() {
+      return googleSheets.getTodos();
     }
   }
 };
 
+// Instagram Example
+// import Instagram from '/imports/data/instagram-connector';
+
+// const resolvers = {
+  // Query: {
+    // async data(root, { keywords }) {
+      // return Instagram.getRecent(keywords);
+    // }
+  // }
+// };
+
 export default resolvers;
+
--- imports/data/schema.js
+++ imports/data/schema.js
@@ -1,13 +1,50 @@
-export default schema = [`
-  type SearchResults {
-    link: String,
-    images: String,
-    caption: String
-  }
-  type Query {
-    data(keywords: String): [SearchResults]
-  }
-  schema {
-    query: Query
-  }
-`];
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLBoolean,
+  GraphQLList
+} from 'graphql';
+import resolvers from './resolvers.js';
+
+
+const TodoType = new GraphQLObjectType({
+  name: 'todo',
+  fields: () => ({
+    id: {
+      type: GraphQLInt,
+      description: 'Todo id'
+    },
+    title: {
+      type: GraphQLString,
+      description: 'Todo title'
+    },
+    completed: {
+      type: GraphQLBoolean,
+      description: 'Flag to mark if the todo is completed'
+    }
+  })
+});
+
+const TodosSchema = new GraphQLObjectType({
+  name: 'TodosSchema',
+  description: "Root of the Todos Schema",
+  fields: () => ({
+    todos: {
+      type: new GraphQLList(TodoType),
+      description: "Array of todos",
+      args: {
+        message: {type: GraphQLString}
+      },
+      // todosExampleResolveFunction should be replaced by the firebase or
+      // google sheets resolve function declared in resolvers.js
+      resolve: resolvers.TodosSchema.todosExampleResolveFunction
+    }
+  })
+});
+
+export default schema = new GraphQLSchema({
+  query: TodosSchema,
+})
+  // mutation: MutationType
